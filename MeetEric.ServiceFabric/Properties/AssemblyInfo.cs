a87b2f7a4d00bac19f32f3efc8e32a7f87760d11
Eric Maino <ericmai@microsoft.com>
2016-10-07 13:40:23 +0800

    Fixing assembly name

--- MeetEric.ServiceFabric/Properties/AssemblyInfo.cs
+++ MeetEric.ServiceFabric/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MeetEric.Adapters.Azure.ServiceFabric")]
+[assembly: AssemblyTitle("MeetEric.ServiceFabric")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MeetEric.Adapters.Azure.ServiceFabric")]
+[assembly: AssemblyProduct("MeetEric.ServiceFabric")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
